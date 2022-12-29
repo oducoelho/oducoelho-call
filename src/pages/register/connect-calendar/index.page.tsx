7999e3a8d62ef0f71a72ce36a3af1ec340361cb6
Eduardo Coelho <eduardocoelho619@gmail.com>
2022-12-29 17:55:52 -0300

    create: creating a login part with google

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,41 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+
+import { ArrowRight } from 'phosphor-react'
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+import { signIn } from 'next-auth/react'
+
+export default function Register() {
+  // async function handleRegister(data: RegisterFormData) {}
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Goggle Calendar</Text>
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
