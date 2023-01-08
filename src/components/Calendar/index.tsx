edb4f91ef0f7f7c700190384f6330372ee82faec
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-01-08 18:34:47 -0300

    Calendar & Availability - Calendar Styling

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -52,6 +52,30 @@ export function Calendar() {
               <CalendarDay>3</CalendarDay>
             </td>
           </tr>
+
+          <tr>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
         </tbody>
       </CalendarBody>
     </CalendarContainer>
